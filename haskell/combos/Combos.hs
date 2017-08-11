28f6b4b11fd7738cfb760484656f8aba1537279c
ojplg <lees@ripco.com>
2017-08-11 16:57:01 -0500

    New file

---
+++ haskell/combos/Combos.hs
@@ -0,0 +1,11 @@
+import Data.List (intersperse)
+
+colors = ["Green","Yellow","Blue","Red","Grey"]
+
+combos =  [[x,",",y,",",z]|x<-colors,y<-colors,z<-colors,x/=y,y/=z,x/=z]
+
+cs = map concat combos
+
+is = intersperse "\n" cs
+
+main = mapM_ putStr is
