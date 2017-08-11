2266ea23cda72f854e7e9afa7eba789529ea1358
ojplg <lees@ripco.com>
2017-08-11 17:11:47 -0500

    Simplify

--- haskell/combos/Combos.hs
+++ haskell/combos/Combos.hs
@@ -4,6 +4,5 @@ import Data.List (intersperse)
 
 colors = ["Green","Yellow","Blue","Red","Grey"]
 
-main = mapM_ putStr 
-          $ intersperse "\n"
-          $ map concat [[x,",",y,",",z]|x<-colors,y<-colors,z<-colors,x/=y,y/=z,x/=z]
+main = mapM_ putStrLn 
+           [x ++ "," ++ y ++ "," ++ z|x<-colors,y<-colors,z<-colors,x/=y,y/=z,x/=z]
