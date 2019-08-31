21bdb5f626736d83666fcb3350afb6d031d50a79
ojplg <lees@ripco.com>
2019-08-31 17:43:27 -0400

    Start magnifying

--- ocaml/numbers/numbers.ml
+++ ocaml/numbers/numbers.ml
@@ -6,7 +6,6 @@
 
 open Core
 
-
 type pattern = {
     top: bool;
     middle: bool;
@@ -42,9 +41,52 @@ let pattern_to_string p =
                     top_left_char; middle_char ; top_right_char; "\n";
                     bottom_left_char; bottom_char; bottom_right_char; "\n";]
 
+let height magnification = 1 + 2 * magnification
+    
+let is_first_row row_number = row_number = 0
+let is_last_row row_number magnification = row_number + 1 = height magnification
+let is_middle_row row_number magnification = row_number = magnification
+let is_top_half row_number magnification = row_number < magnification
+let is_bottom_half row_number magnification = row_number > magnification
+
+let pattern_to_line pattern row_number magnification =
+    if is_first_row row_number then
+        let c = if pattern.top then '_' else ' ' in
+            String.concat [" "; String.make magnification c; " "]
+    else if is_last_row row_number magnification then
+        let c = if pattern.bottom then '_' else ' ' in
+        let s = if pattern.bottom_left then "|" else " " in
+        let e = if pattern.bottom_right then "|" else " " in
+            String.concat [s; String.make magnification c; e]
+    else if is_middle_row row_number magnification then 
+        let c = if pattern.middle then '_' else ' ' in
+        let s = if pattern.top_left then "|" else " " in
+        let e = if pattern.top_right then "|" else " " in
+            String.concat [s; String.make magnification c; e]
+    else ""
+
+let range limit = 
+    let rec range' n ns = 
+        if n = 0 then 0 :: ns
+        else n :: range' (n-1) ns
+    in List.rev (range' limit [])
+
+let pattern_to_lines pattern magnification = 
+    let length = height magnification in
+    let ns = range length in
+    List.map ns (function n -> pattern_to_line pattern n magnification)
+
 let print_pattern p = 
     let s = pattern_to_string p in
     printf "%s\n" s
 
+let print_magnified_pattern pattern magnification =
+    let lines = pattern_to_lines pattern magnification in
+    List.iter lines (function l -> printf "%s\n" l)
+
 let () = 
-    List.iter patterns print_pattern
+    List.iter patterns (function p -> print_magnified_pattern p 1)
+
+let print_magnified_line pattern magnification line_number = 
+    let line = pattern_to_line pattern line_number magnification in
+    printf "%s\n" line
